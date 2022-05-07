af7affc5c7274ab5e4d962f654ef6a8538018145
dianakrupko <dnkrupkoo@gmail.com>
2022-05-08 00:52:02 +0300

    new

---
+++ js/menu.js
@@ -0,0 +1,7 @@
+function openMenu() {
+    document.getElementById("sidebar").classList.toggle('active');
+}
+
+function closeMenu(){
+    document.getElementById("sidebar").classList.toggle('active');
+}
